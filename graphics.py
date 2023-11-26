f9671ee6fac767a6c8308271b294eded51baba81
Derek Marchese <derekmarchese1@gmail.com>
2023-11-25 22:53:39 -0500

    added Line class

--- graphics.py
+++ graphics.py
@@ -13,6 +13,9 @@ class Window:
         self.__root.update_idletasks()
         self.__root.update()
         
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+        
     def wait_for_close(self):
         self.__running = True
         while self.__running:
@@ -28,3 +31,14 @@ class Point:
         self.x = x
         self.y = y
        
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+        
+    def draw(self, canvas, fill_color='black'):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill = fill_color, width=2
+        )
+        canvas.pack(fill=BOTH, expand=1)
